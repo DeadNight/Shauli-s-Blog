43025acdabb1d86482a8e893a93f284ad7c1bb39
Nir Leibovitch <nir.leibo@gmail.com>
2016-08-10 01:07:43 +0300

    create exercise 2

---
+++ Exercise 2/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercise_2.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ActionResult Blog()
+        {
+            ViewBag.Title = "Ha-Blog Shel Shauli";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
