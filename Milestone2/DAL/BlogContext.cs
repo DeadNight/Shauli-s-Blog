2eb4540df4eae33f85dce008454654c287ef3cb7
Nir Leibovitch <nir.leibo@gmail.com>
2016-09-04 19:41:42 +0300

    finish milestone 2

---
+++ Milestone2/DAL/BlogContext.cs
@@ -0,0 +1,18 @@
+﻿using Milestone2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Milestone2.DAL
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(): base("BlogContext")
+        {
+        }
+
+        public DbSet<Fan> Fans { get; set; }
+    }
+}
