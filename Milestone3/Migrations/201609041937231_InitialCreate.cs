5d9605da8dfe9da42ce25a5ca63ca9bf2f56283d
Nir Leibovitch <nir.leibo@gmail.com>
2016-09-04 22:38:54 +0300

    create milestone3

---
+++ Milestone3/Migrations/201609041937231_InitialCreate.cs
@@ -0,0 +1,30 @@
+namespace Milestone3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Fans",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Gender = c.Int(nullable: false),
+                        BirthDate = c.DateTime(nullable: false),
+                        JoinDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Fans");
+        }
+    }
+}
